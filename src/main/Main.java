dab9415e6d8ad977d2ebad5bcbdd2aba7f69dffd
Jose Manuel <mjosev@uninorte.edu.co>
2025-03-21 16:01:19 -0500

    Eliminando repositorio embebido

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main;
-
+import core.Person;
 /**
  *
  * @author Art_Magnus
  */
 public class Main {
-    
+        public static void main(String[] args) {
+            Person person = new Person(15488,"Juan Perez", 23);
+            System.out.println(person);
+        }
 }
+
